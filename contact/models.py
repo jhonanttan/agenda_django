e6dc9ca97d70d644e6dbe4f0928ed9c04860fa62
Jhonanttan <jhonanttan1@gmail.com>
2024-12-14 14:13:27 -0300

    Criando model category(foregein key)

--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-
 # id(primary key - automático)
 # first_name(string), last_name(string), phone(string),
 # email (email), created_date(date), description(text),
@@ -9,6 +8,16 @@ from django.utils import timezone
 
 # depois ->
     # owner(foregein key)
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+    
+    name = models.CharField(max_length=50)
+   
+    def __str__(self) -> str:
+        return self.name
 
 class Contact(models.Model): 
     first_name = models.CharField(max_length=50)
@@ -19,8 +28,11 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
-    
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+        )
     
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name} '
-    
